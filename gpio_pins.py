5dd8528563a10329527c6317f5bdc21f1ccd8f23
Eduardo Ortenblad <edu.ortenblad@gmail.com>
2026-07-19 22:28:02 -0300

    fix: Got basic control working

--- gpio_pins.py
+++ gpio_pins.py
@@ -1,7 +1,4 @@
-from enum import Enum
-
-class GPIO_Pins(Enum):
-    R_MOTOR_IN1 = 26
-    R_MOTOR_IN2 = 19
-    L_MOTOR_IN1 = 13
-    L_MOTOR_IN2 = 6
+R_MOTOR_IN1 = 26
+R_MOTOR_IN2 = 19
+L_MOTOR_IN1 = 13
+L_MOTOR_IN2 = 6
--- rover.py
+++ rover.py
@@ -1,5 +1,4 @@
-from gpiozero import motor
-from gpio_pins import GPIO_Pins
+from gpiozero import Motor
 
 class Rover:
 
@@ -7,20 +6,29 @@ class Rover:
         self._motorR = Motor(forward=in1R, backward=in2R, enable=enableR, pwm=True)
         self._motorL = Motor(forward=in1L, backward=in2L, enable=enableL, pwm=True)
 
-    def forward(speed):
+    def forward(self, speed):
         self._motorR.forward(speed)
         self._motorL.forward(speed)
 
-    def backward(speed):
+    def backward(self, speed):
         self._motorR.backward(speed)
         self._motorL.backward(speed)
 
-    def stop():
+    def right(self, speed):
+        self._motorR.backward(speed)
+        self._motorL.forward(speed)
+
+    def left(self, speed):
+        self._motorR.forward(speed)
+        self._motorL.backward(speed)
+
+    def stop(self):
         self._motorR.stop()
         self._motorL.stop()
 
 
 if __name__ == "__main__":
+    from gpio_pins import R_MOTOR_IN1, R_MOTOR_IN2, L_MOTOR_IN1, L_MOTOR_IN2
     from time import sleep
  
     # For hardware-timed PWM on a Pi 4 or older, uncomment these two lines:
@@ -28,9 +36,18 @@ if __name__ == "__main__":
     # from gpiozero.pins.pigpio import PiGPIOFactory
     # Device.pin_factory = PiGPIOFactory()
 
-    Rover rover(GPIO_Pins.R_MOTOR_IN1, GPIO_Pins.R_MOTOR_IN2, GPIO_Pins.L_MOTOR_IN1, GPIO_Pins.L_MOTOR_IN2)
+    rover = Rover(R_MOTOR_IN1, R_MOTOR_IN2, L_MOTOR_IN1, L_MOTOR_IN2)
+
     rover.forward(1)
     sleep(2)
+
+    rover.right(0.8)
+    sleep(2)
+
+    rover.left(0.8)
+    sleep(2)
+
     rover.backward(1)
     sleep(2)
-    motor.stop()
+
+    rover.stop()
